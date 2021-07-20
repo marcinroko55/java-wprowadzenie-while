3b62b0abad7b7fb239f90cb4948f260f55f341c4
marcinroko55 <79092872+marcinroko55@users.noreply.github.com>
2021-07-20 22:55:00 +0200

    #3 rozwiązanie zadania

--- src/triangleStars.java
+++ src/triangleStars.java
@@ -1,5 +1,18 @@
 public class triangleStars {
     public static void main(String[] args) {
-        
+        char star = '*';
+        int n = 4;
+        int i = 0;
+
+        while (i < n) {
+            int j = 0;
+            while (j < i) {
+                System.out.print(star);
+                j++;
+            }
+            System.out.println(star);
+            i++;
+
+        }
     }
 }
