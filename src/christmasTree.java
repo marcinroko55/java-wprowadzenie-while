fb5eca05c9d1cfb08d59419fdb1aafedf8400758
marcinroko55 <79092872+marcinroko55@users.noreply.github.com>
2021-07-22 18:14:32 +0200

    #5 rozwiązanie zadania

--- src/christmasTree.java
+++ src/christmasTree.java
@@ -1,5 +1,27 @@
 public class christmasTree {
     public static void main(String[] args) {
-        
+        char star = '*';
+        int n = 3;
+        int i = 1;
+        int j = 0;
+
+        while (i <= n) {
+
+            while (j < n - 1) {
+                System.out.print(" ");
+                j++;
+            }
+            j = 1;
+
+            while (j < (i * 2) - 1) {
+
+                System.out.print(star);
+                j++;
+            }
+            System.out.println(star);
+            i++;
+
+        }
     }
 }
+
