ccf23a4fdd28d311ac755690d53729f49b563328
marcinroko55 <79092872+marcinroko55@users.noreply.github.com>
2021-07-29 19:07:59 +0200

    #9 rozpoczęcie ''kodu na piechotę'', zmiana koncepcji

--- src/Sudoku1.java
+++ src/Sudoku1.java
@@ -1,11 +1,68 @@
 public class Sudoku1 {
     public static void main(String[] args) {
 
-        while(){
+        int i = 0;
+        int a = 2;
+        int b = 3;
+        int c = 1;
+
+        int[][] sudokuTable = {
+                {1, 0, 3},
+                {0, 1, 2},
+                {2, 3, 0},
 
 
-            
+        };
+
+
+        while (sudokuTable[0][0] + sudokuTable[0][1] + sudokuTable[0][2] != 6) {
+            int szukanePole = sudokuTable[0][0];
+            if (szukanePole == 0) {
+                int suma = sudokuTable[0][1] + sudokuTable[0][2];
+                if (suma == 3) {
+                    szukanePole = 3;
+                }
+                if (suma == 4) {
+                    szukanePole = 2;
+                }
+                if (suma == 5) {
+                    szukanePole = 1;
+                }
+            }
+            szukanePole = sudokuTable[0][1];
+            if (szukanePole == 0) {
+                int suma = sudokuTable[0][0] + sudokuTable[0][2];
+                if (suma == 3) {
+                    szukanePole = 3;
+                }
+                if (suma == 4) {
+                    szukanePole = 2;
+                }
+                if (suma == 5) {
+                    szukanePole = 1;
+                }
+            }
+            szukanePole = sudokuTable[0][2];
+            if (szukanePole == 0) {
+                int suma = sudokuTable[0][0] + sudokuTable[0][1];
+                if (suma == 3) {
+                    szukanePole = 3;
+                }
+                if (suma == 4) {
+                    szukanePole = 2;
+                }
+                if (suma == 5) {
+                    szukanePole = 1;
+                }
+            }
+
+
         }
 
     }
 }
+//pobieram 3 wartości z pierwszego wiersza i jeśli znajduje się w nim 0, to porównuję ze sobą pozostałe wartości.
+//Jeśli suma wartości wynosi 3, to przypisz 3, jeśli suma wynosi 4, to przypisz 2, jeśli suma wynosi 5, to przypisz 1
+//
+// PÓŹNIEJ ZROBIC GENERYCZNIE
+//zobaczyć co się zmienia i zamiast tego przypisać zmienne, które później będę iterował, najpierw spróbować zmienić indeks kolumn
