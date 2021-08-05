fe8e67de37baa8885acbfd9e7cd8e3d7acfc96e3
marcinroko55 <79092872+marcinroko55@users.noreply.github.com>
2021-08-05 18:45:19 +0200

    #9 Rozwiązanie zadania

--- src/Sudoku1.java
+++ src/Sudoku1.java
@@ -2,67 +2,35 @@ public class Sudoku1 {
     public static void main(String[] args) {
 
         int i = 0;
-        int a = 2;
-        int b = 3;
-        int c = 1;
-
         int[][] sudokuTable = {
                 {1, 0, 3},
                 {0, 1, 2},
                 {2, 3, 0},
-
-
         };
 
-
-        while (sudokuTable[0][0] + sudokuTable[0][1] + sudokuTable[0][2] != 6) {
-            int szukanePole = sudokuTable[0][0];
-            if (szukanePole == 0) {
-                int suma = sudokuTable[0][1] + sudokuTable[0][2];
-                if (suma == 3) {
-                    szukanePole = 3;
+        while (i < sudokuTable.length) {
+            int j = 0;
+            while (j < sudokuTable.length) {
+                if (sudokuTable[i][j] == 0) {
+                    int kolejnyKolejny = (j + 2) % (sudokuTable.length);
+                    int kolejny = (j + 1) % (sudokuTable.length);
+                    int suma = sudokuTable[i][kolejny] + sudokuTable[i][kolejnyKolejny];
+                    if (suma == 3) {
+                        sudokuTable[i][j] = 3;
+                    }
+                    if (suma == 4) {
+                        sudokuTable[i][j] = 2;
+                    }
+                    if (suma == 5) {
+                        sudokuTable[i][j] = 1;
+                    }
                 }
-                if (suma == 4) {
-                    szukanePole = 2;
-                }
-                if (suma == 5) {
-                    szukanePole = 1;
-                }
+                System.out.print(sudokuTable[i][j]);
+                j++;
             }
-            szukanePole = sudokuTable[0][1];
-            if (szukanePole == 0) {
-                int suma = sudokuTable[0][0] + sudokuTable[0][2];
-                if (suma == 3) {
-                    szukanePole = 3;
-                }
-                if (suma == 4) {
-                    szukanePole = 2;
-                }
-                if (suma == 5) {
-                    szukanePole = 1;
-                }
-            }
-            szukanePole = sudokuTable[0][2];
-            if (szukanePole == 0) {
-                int suma = sudokuTable[0][0] + sudokuTable[0][1];
-                if (suma == 3) {
-                    szukanePole = 3;
-                }
-                if (suma == 4) {
-                    szukanePole = 2;
-                }
-                if (suma == 5) {
-                    szukanePole = 1;
-                }
-            }
-
-
+            System.out.println();
+            i++;
         }
-
     }
 }
-//pobieram 3 wartości z pierwszego wiersza i jeśli znajduje się w nim 0, to porównuję ze sobą pozostałe wartości.
-//Jeśli suma wartości wynosi 3, to przypisz 3, jeśli suma wynosi 4, to przypisz 2, jeśli suma wynosi 5, to przypisz 1
-//
-// PÓŹNIEJ ZROBIC GENERYCZNIE
-//zobaczyć co się zmienia i zamiast tego przypisać zmienne, które później będę iterował, najpierw spróbować zmienić indeks kolumn
+
